510260f6403af2207c92aa8ed3890c30420edd2e
Galina Ushenko <g.v.ushenko@gmail.com>
2022-10-27 01:32:18 +0300

    animation and hover

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,19 @@
 $(document).ready(function () {
 
+    new WOW ({
+        animateClass: 'animate__animated',
+    }).init();
+
     document.getElementById('footer-name').onclick = function () {
         document.getElementsByClassName('header')[0].scrollIntoView({behavior: "smooth"});
     }
 
     document.getElementById('contact-btn').onclick = function () {
         document.getElementsByClassName('contact')[0].scrollIntoView({behavior: "smooth"});
+    }
+
+    document.getElementById('portfolio-btn').onclick = function () {
+        document.getElementsByClassName('portfolio')[0].scrollIntoView({behavior: "smooth"});
     }
 
 })
